1dac3a0454a433b97de1b25418513bd7cfc40413
mstrey <mstrey@gmail.com>
2013-09-13 20:41:02 -0300

    refactor of download script

--- server/loadResults.sh
+++ server/loadResults.sh
@@ -6,16 +6,11 @@ declare -a loterias="megase lotfac quina lotoma dplsen timema"
 #go to directory
 cd ~/www/loto/cef
 
-# download results
+# download and unzip results
 for lot in ${loterias[@]}
 do
-        wget -N http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_$lot.zip
-done
-
-# unzip the file
-for lot in ${loterias[@]}
-do
-        unzip -o D_$lot.zip
+	wget -N http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_$lot.zip
+	unzip -o D_$lot.zip
 done
 
 #clean
