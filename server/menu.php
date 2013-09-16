463bb76ce5ae1b6b32249972fe69c9605202d1d9
Maicon Strey <mstrey@gmail.com>
2013-09-16 09:29:31 -0300

    bugfix

--- server/menu.php
+++ server/menu.php
@@ -2,7 +2,7 @@
 function writeMenu(){
   echo '<br/>';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=mega">MegaSena</a> - ';
-  echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=lotofacil">Loto F�cil</a> - ';
+  echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=lotofacil">Loto Fácil</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=quina">Quina</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=lotomania">Loto Mania</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=dupla">Dupla Sena</a> - ';
