8327aa0d05ba0ce035cc2eb65aebe4e454039aad
Maicon Strey <mstrey@gmail.com>
2013-09-16 09:28:35 -0300

    include separator on menu

--- server/menu.php
+++ server/menu.php
@@ -6,7 +6,7 @@ function writeMenu(){
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=quina">Quina</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=lotomania">Loto Mania</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=dupla">Dupla Sena</a> - ';
-  echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=time">Time Mania</a>';
+  echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=time">Time Mania</a>' - ;
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=todos">Todos</a>';
   echo '<br/><br/>';
 }
