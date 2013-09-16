8dddd3f24b1ee35e081845cf6fc41d38dd3711b7
Maicon Strey <mstrey@gmail.com>
2013-09-16 09:26:51 -0300

    include option to parse all games

--- server/menu.php
+++ server/menu.php
@@ -2,11 +2,12 @@
 function writeMenu(){
   echo '<br/>';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=mega">MegaSena</a> - ';
-  echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=lotofacil">Loto Fácil</a> - ';
+  echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=lotofacil">Loto Fï¿½cil</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=quina">Quina</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=lotomania">Loto Mania</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=dupla">Dupla Sena</a> - ';
   echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=time">Time Mania</a>';
+  echo '<a href="http://maicon.strey.nom.br/loto/getResults.php?loto=todos">Todos</a>';
   echo '<br/><br/>';
 }
 ?>
