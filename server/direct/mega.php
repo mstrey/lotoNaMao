965f71fbef8309f6fed4b7fbfc340b5a44b6d1d5
Maicon Strey <mstrey@gmail.com>
2014-04-29 17:20:57 -0300

    Update mega.php

--- server/direct/mega.php
+++ server/direct/mega.php
@@ -29,11 +29,10 @@ function parseXml($content, $node){
 function parseResult($url){
 
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_HEADER, 0);
-	curl_setopt($ch, CURLOPT_VERBOSE, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
-	curl_setopt($ch, CURLOPT_URL, urlencode($url));
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 	$data = curl_exec($ch);
 	curl_close($ch);
 
