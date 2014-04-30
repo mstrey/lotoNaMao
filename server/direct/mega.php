37cdcad3401ad2a17723fe2514a6b2903ef69933
Maicon Strey <mstrey@gmail.com>
2014-04-30 13:43:15 -0300

    Update mega.php

--- server/direct/mega.php
+++ server/direct/mega.php
@@ -32,7 +32,7 @@ function parseResult($url){
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  
         curl_setopt($ch, CURLOPT_URL, $url);  
         curl_setopt($ch, CURLOPT_TIMEOUT, '10');  
-	curl_setopt($curl_handle, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 	$data = curl_exec($ch);  
         curl_close($ch);  
 
