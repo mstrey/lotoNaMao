35449666e1c31a1f22ba1f646ab20481c84f4c0f
Maicon Strey <maicon@localhost.localdomain>
2015-12-07 16:03:02 -0200

    teste com curl usando cookie para acesso ao site da cef

--- server/direct/megasena.php
+++ server/direct/megasena.php
@@ -28,41 +28,60 @@ $server -> handle();
  */
 class WsMegaSena {
 	public function getResultado($concurso) {
-		$urlMega = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp?submeteu=sim";
+		$urlMega = "http://www1.caixa.gov.br/loterias/loterias/megasena/megasena_pesquisa_new.asp";
+		// $urlMega .= "?submeteu=sim\&opcao=concurso\&txtConcurso=1765";
 		$postFields = array(
 						'submeteu' => 'sim'
 						,'opcao' => 'concurso'
 						,'txtConcurso' => '1765'
 					);
-		// $urlMega .= "\&opcao=concurso\&txtConcurso=1765";
-		echo $urlMega;
+
+		// Define a URL original
+		curl_setopt($ch, CURLOPT_URL,$urlMega);
 		
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $urlMega);
+		// Imita o comportamento padrão dos navegadores: manipular cookies 
+		curl_setopt ($ch, CURLOPT_COOKIEJAR, 'cookie.txt'); 
+		
+		// Executa a requisição 
+		$store = curl_exec ($ch); 
+		
+		// Define a URL vedadeira para ser chamada
+		curl_setopt($ch, CURLOPT_URL,$urlMega);
+		// define parametros post do concurso que será recuperado
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
-
+		
+		// Define o tipo de transferência (Padrão: 1) e o tempo (10)
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-		curl_setopt($ch, CURLOPT_USERAGENT, "Mozilome/44.0.2403.125 Safari/537.36");
+		curl_setopt($ch, CURLOPT_TIMEOUT, 10); 
+		
+		// Executa a segunda requisição
+		$result = curl_exec($ch); 
+		
+		// $ch = curl_init();
+		// curl_setopt($ch, CURLOPT_URL, $urlMega);
+// 
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		// curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		// curl_setopt($ch, CURLOPT_USERAGENT, "Mozilome/44.0.2403.125 Safari/537.36");
 		//curl_setopt($ch, CURLOPT_ENCODING, "gzip, deflate, sdch");
-		$header = array(
-				'Accept-Language:pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4'
+		// $header = array(
+				// 'Accept-Language:pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4'
 //				, 'Cache-Control:max-age=0'
 //				, 'Cookie:security=true; ASPSESSIONIDQQSRSQTC=LDJKBNOAEMFFEJMKINMFMAKC'
 //				, 'Host:www1.caixa.gov.br'
 //				, 'Proxy-Connection:keep-alive'
 //				, 'Upgrade-Insecure-Requests:1'
-								);
+								// );
 // --compressed
-    curl_setopt($ch, CURLOPT_HEADER, $header);
+    // curl_setopt($ch, CURLOPT_HEADER, $header);
 
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-
-    $output = curl_exec($ch);
+	// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+	// curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+// 
+    // $output = curl_exec($ch);
 		curl_close($ch);
-		return $output;
+		return $result;
 		
 	}
 
