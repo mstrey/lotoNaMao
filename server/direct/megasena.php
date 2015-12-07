65f929c4cff39013946cf132bf161d971b8201e5
Maicon Strey <maicon@localhost.localdomain>
2015-12-07 16:15:09 -0200

    teste com curl usando cookie para acesso ao site da cef

--- server/direct/megasena.php
+++ server/direct/megasena.php
@@ -49,6 +49,7 @@ class WsMegaSena {
 	    curl_setopt($ch, CURLOPT_HEADER, array(
 				'Accept-Language:pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4'
 				, 'Cache-Control:max-age=0'
+				, 'Proxy-Connection:keep-alive'
 				)
 			);
 				
